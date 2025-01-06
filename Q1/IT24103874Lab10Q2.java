289b63a2e9a5929ee4e1282a773c2d536e77032f
IT24103874 <it24103874@my.sliit.lk>
2025-01-06 14:57:34 +0530

    Update IT24103874Lab10Q2.java

--- Q1/IT24103874Lab10Q2.java
+++ Q1/IT24103874Lab10Q2.java
@@ -9,23 +9,23 @@ public class IT24103874Lab10Q2 {
         System.out.println();
         System.out.println("Marks are valid");
 
-        System.out.print("Enter the mark correspondant grade : ");
-        char grade = input.next().charAt(0);
-        char aGrade;
-
-        if (mark > 75) {
-            aGrade = 'A';
-        } else if (mark > 60){
-            aGrade = 'B';
-        } else if (mark > 50){
-            aGrade = 'C';
-        } else if (mark > 40) {
-            aGrade = 'D';
+        if (mark >= 75) {
+            Grade = 'A';
+        } else if (mark >= 60){
+            Grade = 'B';
+        } else if (mark >= 50){
+            Grade = 'C';
+        } else if (mark >= 40) {
+            Grade = 'D';
         } else {
-            aGrade = 'F';
+            Grade = 'F';
         }
 
-        assert grade==aGrade : "Incorrect grade assigned";
+        assert (Grade==A && mark>=75 && mark<=100 ||
+                Grade==B && mark>=60 && mark<75 ||
+                Grade==C && mark>=50 && mark<60 ||
+                Grade==D && mark>=40 && mark<50 ||
+                Grade==F && mark>=0 && mark<40) : "Incorrect grade assigned";
 
         System.out.println();
         System.out.println("Valid grade assigned");
